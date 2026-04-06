65a62324d2df4ed272c01c7fc3d16b7f14885ae7
gunaseelansofficial <gunaseelansofficial@gmail.com>
2026-04-06 18:12:34 +0530

    deployed

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/api/contact', async (req, res) => {
 // Serve static frontend
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
